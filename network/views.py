84ababbe39ec33cdf96ad05e9f6a17b60deaeb9e
Dmitry <dima.soin2014@yandex.ru>
2026-03-10 21:04:19 +0300

    Добавил ViewSet для Network

--- network/views.py
+++ network/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from network.models import Network
+from network.serializers import NetworkSerializer
 
-# Create your views here.
+
+class NetworkViewSet(viewsets.ModelViewSet):
+    # Откуда брать данные
+    queryset = Network.objects.all()
+    # Какой сериализатор использовать
+    serializer_class = NetworkSerializer
+    # Права доступа только для сотрудников
+    permission_classes = []
+
